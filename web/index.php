fae2cc08f293f16cbdc480c1eb4d6474810500db
Aleksej Sokolov <aleksej000@gmail.com>
2023-11-10 23:31:37 +0200

    Visual changes ++

--- web/index.php
+++ web/index.php
@@ -153,7 +153,7 @@ if (!empty($_POST['name']) && !empty($_POST['value']) && !empty($_POST['days']))
             margin-top: 7%;
             margin-left: 50px;
             width: 90%;
-            max-width: 400px;
+            max-width: 600px;
         }
         
         input {
